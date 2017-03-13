621243bafd3f1a755d35eb8beb74459077c2a021
Mitch Guzman <mguzman9294@gmail.com>
2017-03-12 19:17:56 -0700

    Can get users to display now.

--- SnapchatClone/SelectUserViewController.swift
+++ SnapchatClone/SelectUserViewController.swift
@@ -7,10 +7,13 @@
 //
 
 import UIKit
+import Firebase
 
 class SelectUserViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var users : [User] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,14 +22,42 @@ class SelectUserViewController: UIViewController, UITableViewDataSource, UITable
         
         tableView.dataSource = self
         tableView.delegate = self
+        
+        FIRDatabase.database().reference().child("users").observe(FIRDataEventType.childAdded, with: {(snapshot) in
+        print(snapshot.childSnapshot(forPath: "email"))
+            let snapshotEmail = snapshot.childSnapshot(forPath: "email")
+        
+            /* Had to come up with some clever string manipulation to get emails displayed properly.
+            Probably a better way to do it, but this works
+            */
+            
+            let user = User()
+            let fullEmail = "\(snapshotEmail)"
+            
+            user.email = fullEmail.replacingOccurrences(of: "Snap (email) ", with: "")
+            
+            user.uid = snapshot.key
+            
+            self.users.append(user)
+            self.tableView.reloadData()
+        
+        })
+        
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return users.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        return UITableViewCell()
+        let cell = UITableViewCell()
+        
+        let user = users[indexPath.row]
+        
+        cell.textLabel?.text = user.email
+        
+        return cell
     }
 
 
---
+++ SnapchatClone/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  SnapchatClone
+//
+//  Created by Mitch Guzman on 3/12/17.
+//  Copyright © 2017 Mitch Guzman. All rights reserved.
+//
+
+import Foundation
+
+class User {
+    var email = ""
+    var uid = ""
+}
